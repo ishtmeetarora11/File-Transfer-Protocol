a10e95b0cdd4f74f568475c1de8bcfc7ae12db26
Ishtmeet Singh arora <ishtmeet@Ishtmeets-MacBook-Air.local>
2024-02-22 15:03:25 -0500

    Client Get Working, Need to fix Client send and Server recieve

---
+++ client.java
@@ -0,0 +1,227 @@
+
+
+import java.net.*;
+import java.io.*;
+
+/**
+ * Ftp client class that will connect to the server.
+ */
+public class client {
+    public static void main(String args[]) throws Exception {
+        while (true) {
+            try {
+                System.out.println("Enter the command as : ftpclient <port>");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                String command = bufferedReader.readLine();
+                String[] choice = command.split(" ");
+                if (choice[0].compareTo("ftpclient") == 0) {
+                    // Socket serverSocket = new Socket("localhost", 4000);
+                    // String ip = choice[1];
+                    if (choice.length != 1) {
+                        int port = Integer.parseInt(choice[1]);
+                        if (port == 4000) {
+                            Socket serverSocket = new Socket("localhost", port);
+                            ClientRun client = new ClientRun(serverSocket);
+                            client.initiateClient();
+                        } else {
+                            System.out.println("Wrong port");
+                        }
+                    }
+                    else {
+                        System.out.println("No port given");
+                    }
+                } else {
+                    System.out.println("Type correct command");
+                }
+                // Socket serverSocket = new Socket("localhost", 4000);
+                // CNFTPClientRun client = new CNFTPClientRun(serverSocket);
+                // client.initiateClient();
+            } catch (NumberFormatException ex) {
+                System.out.println("Only enter ftpclient and the port");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+/**
+ * Client socket class that will get connected to the server.
+ */
+class ClientRun {
+    Socket clientSocket;
+    DataInputStream inputStream;
+    DataOutputStream outputStream;
+    BufferedReader bufferedReader;
+    String dir = "";
+
+    ClientRun(Socket soc) {
+        try {
+            clientSocket = soc;
+            inputStream = new DataInputStream(clientSocket.getInputStream());
+            outputStream = new DataOutputStream(clientSocket.getOutputStream());
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        } catch (Exception ex) {
+
+        }
+    }
+
+    /**
+     * Initiates the client
+     *
+     * @throws Exception
+     */
+    public void initiateClient() throws Exception {
+        displayMenu();
+    }
+
+    /**
+     * Displays the list of operations that can be performed by each of the client
+     *
+     * @throws Exception
+     */
+    private void displayMenu() throws Exception {
+        while (true) {
+            System.out.println(">>>>Valid Commands <<<<");
+            System.out.println("2. get filename");
+            System.out.println("3. upload filename");
+            System.out.println("4. exit");
+            System.out.print("\nEnter command :");
+            String command = bufferedReader.readLine();
+            String[] choice = command.split(" ");
+            if ((choice[0].compareTo("upload") == 0 ||
+                    choice[0].compareTo("get") == 0) &&
+                    choice.length == 1) {
+                System.out.println("Please enter the filename along with command");
+                continue;
+            }
+            switch (choice[0]) {
+                case "upload":
+                    outputStream.writeUTF("upload");
+                    uploadFileToServer(choice[1]);
+                    continue;
+                case "get":
+                    outputStream.writeUTF("get");
+                    getFileFromServer(choice[1]);
+                    continue;
+                case "exit":
+                    outputStream.writeUTF("exit");
+                    System.exit(1);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again");
+                    continue;
+            }
+        }
+    }
+
+    /**
+     * Get the file from the server which will be downloaded by the client
+     *
+     * @param fileToReceive downloaded file
+     * @throws Exception
+     */
+    void getFileFromServer(String fileToReceive) throws Exception {
+        outputStream.writeUTF(fileToReceive);
+        String msgFromServer = inputStream.readUTF();
+
+        if (msgFromServer.compareTo("File Not Found") == 0) {
+            System.out.println("File not found on Server ...");
+            return;
+        } else if (msgFromServer.compareTo("READY") == 0) {
+            System.out.println("File found on server");
+            System.out.println("Receiving File ...");
+            File fileAtClient = new File("new"+fileToReceive);
+            if (fileAtClient.exists()) {
+                String option;
+                System.out.println("File already exists at the client." +
+                        " Want to OverWrite (Y/N) ?");
+                option = bufferedReader.readLine();
+                if (option == "N") {
+                    outputStream.flush();
+                    return;
+                }
+            }
+            writeFile(fileAtClient);
+        }
+    }
+
+    /**
+     * Writes the file that is downloaded by the client in the disk of the client.
+     *
+     * @param file name of the downloaded file
+     * @throws Exception
+     */
+    void writeFile(File file) throws Exception {
+        FileOutputStream fout = new FileOutputStream(file);
+            byte[] buffer = new byte[1024]; // Buffer to read chunks of data
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                fout.write(buffer, 0, bytesRead);
+                fout.flush();
+                if (new String(buffer, 0, bytesRead).endsWith("EOF")) {
+                    break;
+                }
+            }
+            fout.close();
+            System.out.println("File transfer complete");
+    }
+
+    /**
+     * Uploads the file to the server.
+     *
+     * @param clientFilename filename that will be uploaded
+     * @throws Exception
+     */
+    void uploadFileToServer(String clientFilename) throws Exception {
+        String serverFileName;
+
+        File fileToSend = new File(clientFilename);
+        if (!fileToSend.exists()) {
+            System.out.println("File does not Exist in client...");
+            outputStream.writeUTF("File not found");
+            return;
+        }
+        serverFileName = clientFilename;
+        outputStream.writeUTF(serverFileName);
+
+        String msgFromServer = inputStream.readUTF();
+        if (msgFromServer.compareTo("File already exists") == 0) {
+            String option;
+            System.out.println("File already exists at the server. Want to OverWrite (Y/N) ?");
+            option = bufferedReader.readLine();
+            if (option.equalsIgnoreCase("Y")) {
+                outputStream.writeUTF("Y");
+            } else {
+                outputStream.writeUTF("N");
+                return;
+            }
+        }
+
+        System.out.println("Uploading File ...");
+        readFile(fileToSend);
+        //System.out.println(inputStream.readUTF());
+    }
+
+    /**
+     * Reads the file to upload it to the server by the client
+     * 
+     * @param fileToSend filename that will be uploaded
+     * @throws Exception
+     */
+    private void readFile(File fileToSend) {
+       try{ FileInputStream fin = new FileInputStream(fileToSend);
+        byte[] buffer = new byte[1024]; // Adjust buffer size as needed
+        int bytesRead;
+    
+        while ((bytesRead = fin.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+            outputStream.flush();
+        }
+        fin.close();
+    }catch(Exception e){
+        System.out.println(e.getStackTrace());
+    }
+    
+    }
+}
---
+++ server.java
@@ -0,0 +1,180 @@
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Ftp Server class that accepts clients and spawns a new thread for each
+ * client.
+ */
+public class server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket(4000);
+        System.out.println("Server Started on Port Number 4000");
+        System.out.println("The server is running.");
+        System.out.println("Waiting for Connection ...");
+        ServerRun ServerRun = new ServerRun(server.accept());
+    }
+}
+
+/**
+ * Ftp server thread class 
+ */
+class ServerRun extends Thread {
+    Socket clientSocket;
+    DataInputStream inputStream;
+    DataOutputStream outputStream;
+    int clientNum;
+    String clientName;
+    String sharedPath = "";
+
+    /**
+     * Instantiates Server thread and accepts new client socket.
+     *
+     * @param soc        client socket
+     * @param _clientNum client id
+     */
+    ServerRun(Socket soc) {
+        try {
+            clientSocket = soc;
+            inputStream = new DataInputStream(clientSocket.getInputStream());
+            outputStream = new DataOutputStream(clientSocket.getOutputStream());
+            System.out.println("Client connected ");
+            start();
+
+        } catch (Exception ex) {
+        }
+    }
+
+    /**
+     * Overrides run method of the thread class to spawn new thread.
+     */
+    public void run() {
+        // authenticateClient();
+        try {
+            while (true) {
+                try {
+                    String messageFromClient = inputStream.readUTF();
+                    switch (messageFromClient) {
+                        case "get":
+                            sendFileToClient();
+                            continue;
+                        case "upload":
+                            getFileFromClient();
+                            continue;
+                        case "exit":
+                            System.out.println("client exits");
+                            continue;
+                    }
+                } catch (ClassNotFoundException classNot) {
+                    System.err.println("Data received in unknown format");
+                }
+            }
+
+            // System.out.println("Total clients connected now:" + clientNum);
+        } catch (IOException ioException) {
+            System.out.println("Disconnect with Client ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close connections
+            try {
+                inputStream.close();
+                outputStream.close();
+                clientSocket.close();
+            } catch (IOException ioException) {
+                System.out.println("Disconnect with Client ");
+            }
+        }
+    }
+
+    /**
+     * Performs upload operation from clients.
+     *
+     * @throws Exception
+     */
+    private void getFileFromClient() throws Exception {
+        String fileFromClient = inputStream.readUTF();
+        if (fileFromClient.compareTo("File not found") == 0) {
+            return;
+        }
+        File fileToSave = new File("new" + fileFromClient);
+        String option;
+
+        if (fileToSave.exists()) {
+            outputStream.writeUTF("File already exists");
+            option = inputStream.readUTF();
+        } else {
+            outputStream.writeUTF("SendFile");
+            option = "Y";
+        }
+
+        if (option.compareTo("Y") == 0) {
+            writeFile(fileToSave);
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Writes the file that is uploaded by the client in the disk of the server.
+     *
+     * @param fileToSave file to save in the server
+     * @throws Exception
+     */
+    private void writeFile(File fileToSave) throws Exception {
+        FileOutputStream fileOutputStream = new FileOutputStream(fileToSave);
+        byte[] buffer = new byte[1024]; // Buffer to read chunks of data
+
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            fileOutputStream.write(buffer, 0, bytesRead); // Write the read chunk to the file
+        }
+
+        fileOutputStream.close();
+
+        String responseFromServer = "File received at server successfully"
+                + " for client " + clientName;
+        System.out.println(responseFromServer);
+        outputStream.writeUTF(responseFromServer);
+    }
+
+    /**
+     * Sends the file to the client which will be downloaded by the client.
+     *
+     * @throws Exception
+     */
+    private void sendFileToClient() throws Exception {
+        File fileToSend = new File(inputStream.readUTF());
+        if (!fileToSend.exists()) {
+            outputStream.writeUTF("File Not Found");
+            return;
+        } else {
+            outputStream.writeUTF("READY");
+            System.out.println("Uploading file...");
+            readfile(fileToSend);
+            System.out.println("File sent from server for client " + clientName);
+            //outputStream.writeUTF("File received successfully");
+            
+        }
+    }
+
+    /**
+     * reads the file which will be downloaded by the client.
+     *
+     * @param file file to be downloaded by the client
+     * @throws Exception
+     */
+    private void readfile(File file) throws Exception {
+        FileInputStream fin = new FileInputStream(file);
+        byte[] buffer = new byte[1024]; // Adjust buffer size as needed
+        int bytesRead;
+        while ((bytesRead = fin.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+            outputStream.flush();
+        }
+        outputStream.writeUTF("EOF");
+        outputStream.flush();
+        fin.close();
+    }
+}
